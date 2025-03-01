6d9bb346e3c850ece5b024e4f9a783f8ca1d4d67
Mihai <Mihai@DESKTOP-3IARAUL>
2025-03-01 21:21:52 +0200

    Add project files.

---
+++ ProiectComponente/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectComponente
+{
+    class Calculator
+    {
+        string denumire;
+        string producator;
+        string CPU; 
+        string GPU;
+        int capacitateRAM;
+        int capacitateStocare;
+        string carcasa;
+        int sursa;
+
+        public Calculator(string _denumire="", string _producator = "", string _CPU = "", string _GPU = "", int _capacitateRAM = 0, int _capacitateStocare = 0, string _carcasa = "", int _sursa = 0)
+        {
+            denumire = _denumire;
+            producator = _producator;
+            CPU = _CPU;
+            GPU = _GPU;
+            capacitateRAM = _capacitateRAM;
+            capacitateStocare = _capacitateStocare;
+            carcasa = _carcasa;
+            sursa = _sursa;
+        }
+    }
+}
---
+++ ProiectComponente/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectComponente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User ex = new User("Mihai", "Florea", "email@email.com", "adresa1", "075994356");
+            Calculator ex1 = new Calculator();
+        }
+
+    }
+}
