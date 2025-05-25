58b1ad8bc341efa40bc605bf40e3a78d8004489b
Florea Mihai <mohaimihai22@gmail.com>
2025-05-25 21:52:23 +0300

    locatie fisier

--- ComponenteProiect/StorageFactory.cs
+++ ComponenteProiect/StorageFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ namespace ComponenteProiect
     
     public class StorageFactory
     {
-        const string numeFisierCalc = "C:\\Users\\Mihai\\source\\repos\\ProiectComponente\\ProiectComponente\\bin\\Debug\\Calculatoare.txt";
-        const string numeFisierUser = "C:\\Users\\Mihai\\source\\repos\\ProiectComponente\\ProiectComponente\\bin\\Debug\\Useri.txt";
-        
+        static string locatieFisierSolutie = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        //const string numeFisierCalc = "C:\\Users\\Mihai\\source\\repos\\ProiectComponente\\ProiectComponente\\bin\\Debug\\Calculatoare.txt";
+        //const string numeFisierUser = "C:\\Users\\Mihai\\source\\repos\\ProiectComponente\\ProiectComponente\\bin\\Debug\\Useri.txt";
+        static string numeFisierCalc = Path.Combine(locatieFisierSolutie, "Calculatoare.txt");
+        static string numeFisierUser = Path.Combine(locatieFisierSolutie, "Useri.txt");
         public StorageFactory()
         { 
         
